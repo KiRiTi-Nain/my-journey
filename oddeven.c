68a194828ca5e9b694701a47425bc55c2a7c2357
KiRiTi-Nain <KiRiTi-Nain@github.com>
2023-06-04 13:28:40 +0530

    8

--- oddeven.c
+++ oddeven.c
@@ -5,7 +5,7 @@ int n;
 	 printf("\n Enter any number:");
 		scanf("%d",&n);
 	if (n%2==0)
-	printf("The number is even\n");
+	printf("\nThe number is even\n");
 	else
 	printf("The nmuber is odd\n");
 	return 0;

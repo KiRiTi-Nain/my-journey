99e0230187d4601f4f5b9a401d36f075c40f8966
KiRiTi-Nain <KiRiTi-Nain@github.com>
2023-06-04 13:23:07 +0530

    aisehi

--- oddeven.c
+++ oddeven.c
@@ -2,11 +2,11 @@
 int main()
 {
 int n;
-printf("\n Enter any number:");
-scanf("%d",&n);
-if (n%2==0)
-printf("The number is even\n");
-else
-printf("The nmuber is odd\n");
-return 0;
+	printf("\n Enter any number:");
+	scanf("%d",&n);
+	if (n%2==0)
+	printf("The number is even\n");
+	else
+	printf("The nmuber is odd\n");
+	return 0;
 }
